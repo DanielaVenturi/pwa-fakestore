87ebea561d027a7ef522a310b9219477ddbe9aa2
DanielaVenturi <danielaventuri14@gmail.com>
2024-03-11 11:56:10 -0300

    Responsividade com componentes dinâmicos e desafio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+  import ListagemProdutos from '@/components/ListagemProdutos.vue';
 </script>
 
 <template>
-<p></p>
+  <ListagemProdutos />
 </template>
+
+<style scoped></style>
